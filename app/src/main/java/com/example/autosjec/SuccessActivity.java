5445adb2e30bef10dc8bbfb6ff839bc45453f5ba
Stalin Prevan Crasta <prevanstalin@gmail.com>
2025-01-25 22:37:05 +0530

    feat: Add ToolBar with navigation, title and save button

--- app/src/main/java/com/example/autosjec/SuccessActivity.java
+++ app/src/main/java/com/example/autosjec/SuccessActivity.java
@@ -1,21 +1,34 @@
 package com.example.autosjec;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class SuccessActivity extends AppCompatActivity {
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success);
 
+        // Setup toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("SJEC Portal");
+
         try {
             String universityNumber = getIntent().getStringExtra("UNIVERSITY_NUMBER");
             String dateOfBirth = getIntent().getStringExtra("DATE_OF_BIRTH");
 
-            WebView webView = new WebView(this);
+            webView = findViewById(R.id.webview);
             webView.getSettings().setJavaScriptEnabled(true);
             webView.setWebViewClient(new WebViewClient() {
                 @Override
@@ -31,7 +44,7 @@ public class SuccessActivity extends AppCompatActivity {
                             "document.getElementById('dd').value = '" + day + "';" +
                             "document.getElementById('mm').value = '" + month + "';" +
                             "document.getElementById('yyyy').value = '" + year + "';" +
-                             "putdate(); " +
+                            "putdate(); " +
                             "document.querySelector('.cn-login-btn').click();", null
                         );
                     } catch (Exception e) {
@@ -39,10 +52,37 @@ public class SuccessActivity extends AppCompatActivity {
                     }
                 }
             });
-            setContentView(webView);
             webView.loadUrl("https://sjecparents.contineo.in/");
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.resource, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+//        if (item.getItemId() == android.R.id.home) {
+//            finish();
+//            return true;
+//        } else
+        if (item.getItemId() == R.id.action_download) {
+            Toast.makeText(this, "Download feature coming soon", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
         }
     }
 }
